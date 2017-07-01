db75d37102a0f91c3f6e8e0e766d489b120355f6
Mauricio Lancheros <lucalan66@gmail.com>
2017-07-01 18:03:49 -0500

    update

--- app/controllers/notes.rb
+++ app/controllers/notes.rb
@@ -15,7 +15,7 @@ end
 #CREATE
 
 post '/notes' do 
-  @note = Note.new(title: params[:title], content: params[:content])
+  @note = Note.new(title: params[:title], content: params[:content]) #por qué le debemos asignar params[:title] a title:
   if @note.save
 	redirect '/notes'
   else
@@ -35,4 +35,12 @@ end
 get '/notes/:id/edit' do
   @note = Note.find_by(id: params[:id])
   erb :edit
+end
+
+#UPDATE
+
+patch '/notes/:id' do
+  @note = Note.find_by(id: params[:id])
+  @note.update(title: params[:title], content: params[:content])
+  redirect "notes/#{@note.id}"
 end
